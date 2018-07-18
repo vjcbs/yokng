b49b32cd63b85f3367153fb8375749f6f3dea7dc
owlish <vaynimble@gmail.com>
2018-07-19 04:06:41 +0600

    Basic decoration

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+  $.each($('.programme__item-icon, .catalog__icon'), function(){
+    if ($(this).data('type') === 'city') {
+      $(this).css({
+        'width': '34px',
+        'height': '33px'
+      })
+    }
+  })
+
   $(window).on('click', function(){
     // let window_size = window.matchMedia('(min-width: 768px)');
     // console.log(window_size.matches);
