698de6d4abac68050e27c4f37d22af3fa466c5ae
owlish <vaynimble@gmail.com>
2018-07-25 16:48:31 +0600

    Drag n drop

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,27 @@
 $(document).ready(function(){
 
-  $.each($('.programme__item-icon, .catalog__icon'), function(){
-    if ($(this).data('type') === 'city') {
-      $(this).css({
-        'width': '34px',
-        'height': '33px'
-      })
-    }
+  // FILTER SWITCH
+  $('.features__filter-link').on('click', function(){
+    $('.features__filter-link').removeClass('features__filter-link--active');
+    $(this).addClass('features__filter-link--active');
   })
 
-  $(window).on('click', function(){
-    // let window_size = window.matchMedia('(min-width: 768px)');
-    // console.log(window_size.matches);
-    // if (window_size.matches) {
-    //   $('.features__desc-icon-wrapper').removeClass('visually-hidden');
-    //   $('.features__item').removeClass('visually-hidden');
-    // }
-    $.each($('.features__img-wrapper'), function(){
-      console.log($(this).height());
-    })
-  });
+  // FILTERING CATALOG
+  $('.catalog__filter-select').on('change', function(){
+    let filterValue = this.value;
 
+    $.each($('.catalog__item'), function(){
+      let item = $(this);
+      if (item.data('placetype') != filterValue) {
+        item.hide();
+      } else {
+        item.show();
+      }
+
+      if (filterValue == 'none') {
+        item.show();
+      }
+    })
+
+  })
 })
